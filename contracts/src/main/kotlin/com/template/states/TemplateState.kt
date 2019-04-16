3f59b01f445f5d57c73e1b4955d5b3480dd1a319
Hristo Gatsinski <h.gatsinski@magicsolutions.bg>
2019-04-16 10:24:26 +0300

    Add basic PropertyState

--- contracts/src/main/kotlin/com/template/states/TemplateState.kt
+++ contracts/src/main/kotlin/com/template/states/TemplateState.kt
@@ -1,12 +1,20 @@
 package com.template.states
 
-import com.template.contracts.TemplateContract
+import com.template.contracts.PropertyContract
 import net.corda.core.contracts.BelongsToContract
 import net.corda.core.contracts.ContractState
-import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.Party
+import java.time.Instant
 
 // *********
 // * State *
 // *********
-@BelongsToContract(TemplateContract::class)
-data class TemplateState(val data: String, override val participants: List<AbstractParty> = listOf()) : ContractState
+@BelongsToContract(PropertyContract::class)
+data class PropertyState(
+    val owners: List<Party>,
+    val tenants: List<Party>,
+    val constructedAt: Instant,
+    val area: Int,
+    val address: String,
+    override val participants: List<Party> = owners
+) : ContractState
